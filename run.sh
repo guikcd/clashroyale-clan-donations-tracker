564fc00376dfc296a7cf09ac28c86e8d63fd2dbc
Guillaume Delacour <guillaume.delacour@fr.clara.net>
2018-10-11 21:12:44 +0200

    Pass env variables at runtime

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker build -t donations:0.1 .
 bash ./tests.sh
-docker run --restart always --link influxdb --name donations --detach donations:0.1
+ docker run --restart always --env CR_API_TOKEN=$CR_API_TOKEN --env INFLUXDB_HOST=influxdb --link influxdb --name donations --detach donations:0.1
 docker logs donations
